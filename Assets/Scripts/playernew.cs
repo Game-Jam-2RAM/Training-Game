299ca03f635ca66fa8fd304aca43083d2436f05b
Riham Katout <rihamkatm@gmail.com>
2025-06-10 02:26:59 +0300

    Fix player movement

--- Assets/Scripts/playernew.cs
+++ Assets/Scripts/playernew.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,17 +19,15 @@ namespace Player
         public float backSpeed = 5f;
         public float runBoost = 5f;
         public float rotationSpeed = 100f;
-        public float jumpSpeed = 5f;
+        public float jumpSpeed = 3f;
         float currentSpeed;
-
-        float jumpBufferTime = 0.2f;
-        float jumpBufferCounter = 0f;
-
-        bool isWalking = false;
+        bool isWalkingForward = false;
         bool isWalkingBack = false;
         bool isRotating = false;
         bool isJumping = false;
-
+        bool isRunning = false;
+        bool isRotatingLeft = false;
+        bool isRotatingRight = false;
         void Start()
         {
             animator = GetComponent<Animator>();
@@ -39,216 +38,117 @@ namespace Player
 
         void Update()
         {
-            HandleRotation();
+            isWalkingForward = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+            isWalkingBack = controller.isGrounded && !Input.GetKey(KeyCode.Space)
+                            && (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow));
+            isRunning = Input.GetKey(KeyCode.LeftShift);
+            currentSpeed = isWalkingBack ? -backSpeed
+                                        : (isWalkingForward ? (walkSpeed + (isRunning ? runBoost : 0))
+                                                    : 0);
+            isRotatingLeft = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
+            isRotatingRight = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
+            isRotating = controller.isGrounded && !Input.GetKey(KeyCode.Space) && (isRotatingLeft || isRotatingRight);
+            isRotatingLeft &= isRotating;
+            isRotatingRight &= isRotating;
+            if (!isJumping && Input.GetKeyDown(KeyCode.Space))
+            {
+                isJumping = true;
+            }
             HandleAnimation();
-            if ((Input.GetKeyDown(KeyCode.Space) && isWalking) || (Input.GetKeyDown(KeyCode.Space) && !isWalking && !isWalkingBack && !isRotating))
-            {
-                jumpBufferCounter = jumpBufferTime; // start buffer timer
-            }
-            else
-            {
-                jumpBufferCounter -= Time.deltaTime;
-            }
+            HandleMovement();
+            if (isRotating) HandleRotation();
+
+
             if (isJumping && controller.isGrounded && moveDirection.y <= 0f)
             {
                 isJumping = false;
                 animator.ResetTrigger("jump"); // <- important
             }
-
-
-
         }
-
-        void FixedUpdate()
-        {
-            HandleMovement();
-        }
-
 
         void HandleMovement()
         {
-            // Reset x and z movement
-            Vector3 horizontalMove = Vector3.zero;
-
-            // Allow forward and backward movement only when grounded
-            if (controller.isGrounded)
+            // Jump
+            if (isJumping)
             {
-                // Grounded movement
-                if (Input.GetKey(KeyCode.W))
-                    horizontalMove = transform.forward * currentSpeed;
-                else if (Input.GetKey(KeyCode.S))
-                    horizontalMove = -transform.forward * backSpeed;
-
-                // Jump
-                if (jumpBufferCounter > 0f && controller.isGrounded && !isJumping)
-                {
-                    isJumping = true;
-                    StartCoroutine(DelayedJump(0.5f)); // Adjust delay (e.g., 0.3 seconds)
-                }
-
-
-            }
-            else
-            {
-                // Retain forward momentum while airborne
-                if (Input.GetKey(KeyCode.W))
-                    horizontalMove = transform.forward * currentSpeed;
+                moveDirection.y = jumpSpeed;
+                moveDirection += transform.forward * currentSpeed;
+                isJumping = false;
             }
 
+            Vector3 horizontalMove = transform.forward * currentSpeed;
             // Apply horizontal movement
             moveDirection.x = horizontalMove.x;
             moveDirection.z = horizontalMove.z;
 
-            // Apply gravity
+            // Apply gravity then move the character
             moveDirection.y -= gravity * Time.deltaTime;
-
-            // Move the character
             controller.Move(moveDirection * Time.deltaTime);
         }
-        IEnumerator DelayedJump(float delay)
-        {
-            yield return new WaitForSeconds(delay);
-
-            moveDirection.y = jumpSpeed;
-
-            if (Input.GetKey(KeyCode.W))
-            {
-                moveDirection += transform.forward * currentSpeed;
-            }
-
-            jumpBufferCounter = 0f;
-        }
-
-
 
 
         void HandleRotation()
         {
-            float turn = 0f;
-            if (Input.GetKey(KeyCode.A) && controller.isGrounded)
-                turn = -rotationSpeed;
-            if (Input.GetKey(KeyCode.D) && controller.isGrounded)
-                turn = rotationSpeed;
-
+            float turn = isRotatingLeft ? -rotationSpeed : rotationSpeed;
             playerTrans.Rotate(0, turn * Time.deltaTime, 0);
-            isRotating = (turn != 0);
         }
 
+        void clearAnimationTriggers()
+        {
+            animator.SetTrigger("idle");
+            animator.ResetTrigger("walk");
+            animator.ResetTrigger("run");
+            animator.ResetTrigger("walkback");
+            animator.ResetTrigger("left");
+            animator.ResetTrigger("right");
+            animator.ResetTrigger("jump");
+        }
         void HandleAnimation()
         {
-            if (Input.GetKeyDown(KeyCode.Space) && controller.isGrounded && !isWalkingBack && !isRotating)
+            clearAnimationTriggers();
+
+            // jump animation - trigger immediately when jumping starts
+            if (isJumping || !controller.isGrounded)
             {
                 animator.SetTrigger("jump");
                 animator.ResetTrigger("idle");
-                animator.ResetTrigger("walk");
-                animator.ResetTrigger("run");
-                animator.ResetTrigger("walkback");
-                animator.ResetTrigger("left");
-                animator.ResetTrigger("right");
-
+                return;
             }
-            if (!isJumping)
+
+            // running animation
+            if (isRunning && isWalkingForward)
             {
-                // Walking Forward
-                if (Input.GetKeyDown(KeyCode.W))
-                {
-                    animator.SetTrigger("walk");
-                    animator.ResetTrigger("idle");
-                    animator.ResetTrigger("left");
-                    animator.ResetTrigger("right");
-                    isWalking = true;
-                    currentSpeed = walkSpeed;
-                }
-                if (Input.GetKeyUp(KeyCode.W))
-                {
-                    animator.ResetTrigger("walk");
-                    isWalking = false;
-
-                    // Only go to idle if not rotating
-                    if (!isRotating)
-                    {
-                        animator.SetTrigger("idle");
-                    }
-                }
-                // Running (Shift)
-                if (isWalking && Input.GetKeyDown(KeyCode.LeftShift) && controller.isGrounded)
-                {
-                    currentSpeed = walkSpeed + runBoost;
-                    animator.SetTrigger("run");
-                    animator.ResetTrigger("walk");
-                }
-                if ((isWalking && Input.GetKeyUp(KeyCode.LeftShift) && controller.isGrounded) || (!Input.GetKey(KeyCode.LeftShift) && isWalking && controller.isGrounded))
-                {
-                    currentSpeed = walkSpeed;
-                    animator.ResetTrigger("run");
-                    animator.SetTrigger("walk");
-                }
+                animator.SetTrigger("run");
+                animator.ResetTrigger("idle");
+                return;
             }
 
+            // Walking Forward
+            else if (isWalkingForward)
+            {
+                animator.SetTrigger("walk");
+                animator.ResetTrigger("idle");
+            }
 
-
-
-            // Walking Backward
-            if (Input.GetKeyDown(KeyCode.S) && controller.isGrounded)
+            // walking backward
+            else if (isWalkingBack)
             {
                 animator.SetTrigger("walkback");
                 animator.ResetTrigger("idle");
-                animator.ResetTrigger("left");
-                animator.ResetTrigger("right");
-                isWalkingBack = true;
-            }
-            if (Input.GetKeyUp(KeyCode.S) && controller.isGrounded)
-            {
-                animator.ResetTrigger("walkback");
-                isWalkingBack = false;
-
-                if (!isRotating)
-                {
-                    animator.SetTrigger("idle");
-                }
             }
 
-            // Rotation Animations
-            if (Input.GetKey(KeyCode.A) && !isWalking && !isWalkingBack && controller.isGrounded)
+            // Left Rotation
+            if (isRotatingLeft)
             {
                 animator.SetTrigger("left");
                 animator.ResetTrigger("idle");
-                animator.ResetTrigger("right");
             }
-            if (Input.GetKeyUp(KeyCode.A) && !isWalking && !isWalkingBack && controller.isGrounded)
-            {
-                animator.ResetTrigger("left");
-                animator.SetTrigger("idle");
-            }
-
-            if (Input.GetKey(KeyCode.D) && !isWalking && !isWalkingBack && controller.isGrounded)
+            // Right Rotation
+            else if (isRotatingRight)
             {
                 animator.SetTrigger("right");
                 animator.ResetTrigger("idle");
-                animator.ResetTrigger("left");
             }
-            if (Input.GetKeyUp(KeyCode.D) && !isWalking && !isWalkingBack && controller.isGrounded)
-            {
-                animator.ResetTrigger("right");
-                animator.SetTrigger("idle");
-            }
-
-            // Handle rotation while walking stops
-            if (isRotating && !isWalking && !isWalkingBack && controller.isGrounded)
-            {
-                if (Input.GetKey(KeyCode.A))
-                {
-                    animator.SetTrigger("left");
-                    animator.ResetTrigger("idle");
-                }
-                else if (Input.GetKey(KeyCode.D))
-                {
-                    animator.SetTrigger("right");
-                    animator.ResetTrigger("idle");
-                }
-            }
-
-
         }
     }
 }
