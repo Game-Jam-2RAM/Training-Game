4174abcb67fb91ea74af31ce8dbc993b4d57821d
MariamAL-batool <130221679+MariamAL-batool@users.noreply.github.com>
2025-06-13 15:55:04 +0300

    monster animation

--- Assets/Scripts/EnemyNavigation.cs
+++ Assets/Scripts/EnemyNavigation.cs
@@ -1,48 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.AI;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.AI;
 
-public class EnemyNavigation : MonoBehaviour
-{
-    public Transform player;
-    private NavMeshAgent agent;
-    private Animator animator;
+// public class EnemyNavigation : MonoBehaviour
+// {
+//     public Transform player;
+//     private NavMeshAgent agent;
+//     private Animator animator;
 
-    private bool isWalking = false;
+//     private bool isWalking = false;
 
-    void Start()
-    {
-        agent = GetComponent<NavMeshAgent>();
-        animator = GetComponent<Animator>();
-    }
+//     void Start()
+//     {
+//         agent = GetComponent<NavMeshAgent>();
+//         animator = GetComponent<Animator>();
+//     }
 
-    void Update()
-    {
-        if (player != null)
-        {
-            agent.SetDestination(player.position);
+//     void Update()
+// {
+//     if (player != null)
+//     {
+//         agent.SetDestination(player.position);
+//         float distance = Vector3.Distance(transform.position, player.position);
 
-            float distance = Vector3.Distance(transform.position, player.position);
-
-            if (distance > 1.5f)
-            {
-                if (!isWalking)
-                {
-                    animator.ResetTrigger("idle");
-                    animator.SetTrigger("walk");
-                    isWalking = true;
-                }
-            }
-            else
-            {
-                if (isWalking)
-                {
-                    animator.ResetTrigger("Walk");
-                    animator.SetTrigger("Idle");
-                    isWalking = false;
-                }
-            }
-        }
-    }
-}
+//         if (distance > 2f) // enemy starts walking if player is far
+//         {
+//             if (!isWalking)
+//             {
+//                 animator.ResetTrigger("idle");
+//                 animator.SetTrigger("walk");
+//                 isWalking = true;
+//             }
+//         }
+//         else
+//         {
+//             if (isWalking)
+//             {
+//                 animator.ResetTrigger("walk");
+//                 animator.SetTrigger("idle");
+//                 isWalking = false;
+//             }
+//         }
+//     }
+// }
+// }
