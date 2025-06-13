d94c30d414946b5d2c75416f41ff1bf71c355a00
Riham Katout <rihamkatm@gmail.com>
2025-06-14 01:59:52 +0300

    Merge

--- Assets/Scripts/Diamond.cs
+++ Assets/Scripts/Diamond.cs
@@ -6,11 +6,11 @@ public class Diamond : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-       PlayerInventory playerInventory = other.GetComponent<PlayerInventory>();
+        PlayerInventory playerInventory = other.GetComponent<PlayerInventory>();
         if (playerInventory != null)
         {
             playerInventory.DiamondCollected();
             gameObject.SetActive(false); // Deactivate the diamond after collection
-       }
- }
+        }
+    }
 }
