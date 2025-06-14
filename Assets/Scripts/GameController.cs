23ec9f379c6b176e931bc5116308a4f8261b201a
Riham Katout <rihamkatm@gmail.com>
2025-06-14 08:42:20 +0300

    Adding game controller script

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    // This script is for controlling sences, background music, and other game-wide settings.
+    // It can also be used to manage game state, such as starting, pausing, and ending the game.
+
+    public static GameController Instance;
+
+    [Header("Audio")]
+    public AudioSource musicSource;
+    public AudioClip backgroundMusic;
+    public int musicVolume = 100;
+
+    private void Awake()
+    {
+        // Singleton logic
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        PlayBackgroundMusic();
+    }
+
+    void PlayBackgroundMusic()
+    {
+        if (musicSource && backgroundMusic)
+        {
+            musicSource.clip = backgroundMusic;
+            musicSource.loop = true;
+            musicSource.volume = musicVolume / 100f;
+            musicSource.Play();
+        }
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SetMusicVolume(int volume)
+    {
+        if (musicSource)
+            musicSource.volume = volume / 100f;
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,21 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    //Start game
     public void Play()
     {
-        SceneManager.LoadScene("SampleScene");
-        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        Debug.Log("Player has start the game.");
+        GameController.Instance.ChangeScene("SampleScene");
     }
-    //Exit game
     public void Quit()
     {
-        Application.Quit();
-        Debug.Log("Player has quit the game.");
+        GameController.Instance.QuitGame();
     }
 }
